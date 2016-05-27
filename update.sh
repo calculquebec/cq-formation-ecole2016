b8d7135846a7b40d1b13fb91427b9b0a3a0b759e
Jean-Francois St-Pierre <jf.stpierre@calculquebec.ca>
2016-05-27 12:03:07 -0400

    Brise le script en deux pour réparer l'autoupdate qui ne pouvait fonctionner autrement

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+pushd $HOME/ecole2016
+
+CONFIGS=(
+	"cq-formation-openacc master" 
+	"cq-formation-advanced-python ulaval"
+	"introduction_R master"
+	"cq-formation-premiers-pas master"
+	"cq-formation-intro-openmp mcgill"
+	"cq-formation-openspeedshop master"
+	"software-carpentry-unix-shell master"
+	)
+
+for i in $(seq 0 $(( ${#CONFIGS[@]} - 1)) ); do 
+  CONFIG=${CONFIGS[$i]}
+  REPO=$(echo $CONFIG | cut -d " " -f 1)
+  BRANCH=$(echo $CONFIG | cut -d " " -f 2)
+  if [[ ! -d $REPO ]]; then
+    git clone https://github.com/calculquebec/$REPO.git
+    pushd $REPO
+    git checkout $BRANCH
+    git pull
+    popd
+  else
+    pushd $REPO
+    git checkout $BRANCH
+    git pull
+    popd
+  fi
+done
+popd
+
